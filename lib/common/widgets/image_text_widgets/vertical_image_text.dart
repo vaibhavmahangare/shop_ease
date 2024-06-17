08ed171941aed3b32650d7104fd3441ba41646b1
vaibhavmahangare <vaibhav.mahangare@mitaoe.ac.in>
2024-06-17 09:43:47 +0530

    homScreen categories

---
+++ lib/common/widgets/image_text_widgets/vertical_image_text.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/sizes.dart';
+import '../../../utils/helpers/helper_functions.dart';
+
+class TVertivalImageText extends StatelessWidget {
+  const TVertivalImageText({
+    super.key, required this.image, required this.title,  this.textColor=TColors.white, this.backgroundColor=TColors.white, this.onTap,
+  });
+
+  final String image,title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final void Function()? onTap;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap:onTap ,
+      child: Padding(
+        padding: const EdgeInsets.only(right: TSizes.spaceBtwItems),
+        child: Column(
+          children: [
+            Container(
+              width: 56,
+              height: 56,
+              padding:const EdgeInsets.all(TSizes.sm),
+              decoration: BoxDecoration(
+                color: backgroundColor??(THelperFunctions.isDarkMode(context)? TColors.black:TColors.white),
+              ),
+              child: Center(
+                child: Image(image: AssetImage(image),fit: BoxFit.cover,color:THelperFunctions.isDarkMode(context)? TColors.light:TColors.dark,),
+              ),
+            ),
+
+            ///---Text---
+            const SizedBox(height: TSizes.spaceBtwItems/2,),
+            SizedBox(width: 55,child: Text(title,style: Theme.of(context).textTheme.labelMedium!.apply(color:textColor),maxLines: 1,overflow: TextOverflow.ellipsis,)),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:shop_ease/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:shop_ease/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:shop_ease/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:shop_ease/utils/device/device_utility.dart';
 import 'package:shop_ease/utils/helpers/helper_functions.dart';
 
@@ -13,6 +14,7 @@ import '../../../../common/widgets/custom_shapes/containers/circular_container.d
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
 import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
 import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
 import '../../../../common/widgets/texts/section_heading.dart';
 import '../../../../utils/constants/colors.dart';
@@ -25,7 +27,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return  const Scaffold(
       body:SingleChildScrollView(
 
         child: Column(
@@ -34,54 +36,23 @@ class HomeScreen extends StatelessWidget {
             child:Column(
               children:[
                 ///--AppBar--
-                const THomeAppBar(),
-                const SizedBox(height: TSizes.spaceBtwSections,),
+                THomeAppBar(),
+                SizedBox(height: TSizes.spaceBtwSections,),
                 ///--SearchBar--
-                const TSearchContainer(text: 'Search In Store',),
-                const SizedBox(height: TSizes.spaceBtwSections,),
+                TSearchContainer(text: 'Search In Store',),
+                SizedBox(height: TSizes.spaceBtwSections,),
 
                 ///---Catagories---
                 Padding(
-                    padding: const EdgeInsets.only(left:TSizes.defaultSpace),
+                    padding: EdgeInsets.only(left:TSizes.defaultSpace),
                     child:Column(
                       children: [
                         ///---Heading---
-                        const TSectionHeading(title:'Popular Catagories' ,showActionButton: false,),
-                        const SizedBox(height: TSizes.spaceBtwSections,),
+                        TSectionHeading(title:'Popular Catagories' ,showActionButton: false,textColor: Colors.white,),
+                        SizedBox(height: TSizes.spaceBtwSections,),
                         
                         ///---Catagories---
-                        SizedBox(
-                          height: 80,
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: 6,
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (_,index){
-                              return Column(
-                                children: [
-                                  Container(
-                                    width: 56,
-                                    height: 56,
-                                    padding:const EdgeInsets.all(TSizes.sm),
-                                    decoration: BoxDecoration(
-                                      color: TColors.white,
-                                      borderRadius: BorderRadius.circular(100),
-                                    ),
-                                    child: const Center(
-                                      child: Image(image: AssetImage(TImages.sportIcon),fit: BoxFit.cover,color: TColors.dark,),
-                                    ),
-                                  ),
-
-                                  ///---Text---
-                                  const SizedBox(height: TSizes.spaceBtwItems/2,),
-                                  Text(data)
-                                  
-
-                                ],
-                              );
-                            },
-                          ),
-                        ),
+                        THomeCategories(),
                       ],
                     ) ,
                 ),
@@ -95,4 +66,5 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
 
