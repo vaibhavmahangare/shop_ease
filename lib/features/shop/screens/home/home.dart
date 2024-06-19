3ff7a706cfc9e6eead9c55653ada592b06ff5c7d
vaibhavmahangare <vaibhav.mahangare@mitaoe.ac.in>
2024-06-19 22:58:15 +0530

    carousel

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,33 +1,22 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:shop_ease/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:shop_ease/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:shop_ease/features/shop/screens/home/widgets/home_categories.dart';
-import 'package:shop_ease/utils/device/device_utility.dart';
-import 'package:shop_ease/utils/helpers/helper_functions.dart';
 
-import '../../../../common/widgets/appbar/appbar.dart';
-import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
-import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
-import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../common/widgets/images/t_rounded_images.dart';
 import '../../../../common/widgets/texts/section_heading.dart';
-import '../../../../utils/constants/colors.dart';
 import '../../../../utils/constants/image_strings.dart';
 import '../../../../utils/constants/sizes.dart';
-import '../../../../utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return  const Scaffold(
       body:SingleChildScrollView(
 
         child: Column(
@@ -64,12 +53,16 @@ class HomeScreen extends StatelessWidget {
             ///Body
             Padding(
               padding: const EdgeInsets.all(TSizes.defaultSpace),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(TSizes.md),
-                ),
-                child: ClipRRect(borderRadius: BorderRadius.circular(TSizes.md),child: Image(image:AssetImage(TImages.banner3))),
-              ),
+              child: CarouselSlider(
+                options:CarouselOptions(
+                    viewportFraction:0.8,)
+    )
+                items:[
+                  TRoundedImage(imageUrl: TImages.banner3,),
+                  TRoundedImage(imageUrl: TImages.banner3,),
+                  TRoundedImage(imageUrl: TImages.banner3,),
+                ]
+              )
             ),
           ],
         ),
@@ -77,5 +70,4 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
 
